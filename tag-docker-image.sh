c555b14f9ac6541ee6ab69552f8149f6daf97472
rmelick-vida <russell@vida.com>
2021-12-21 12:11:13 -0800

    fix image tagging

--- tag-docker-image.sh
+++ tag-docker-image.sh
@@ -1,4 +1,4 @@
 # this script just builds and tags the docker image
-TAG=rmelickvida/public-example-django:$(git rev-parse --short HEAD)
+TAG=rmelickvida/public-example-microservice:$(git rev-parse --short HEAD)
 docker build -t ${TAG} .
 docker push ${TAG}
